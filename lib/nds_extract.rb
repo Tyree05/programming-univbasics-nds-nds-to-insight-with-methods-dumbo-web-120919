a2d7c6dd29758a1b25a6c828c5339572b7bbf664
Tyree Williams <tyreewilliams515@gmail.com>
2019-12-02 19:27:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,11 +6,36 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-  result = {}
+  row_index = 0
+  directors_total = []
+  while row_index < nds.length do
+    total = 0
+    total = gross_for_director(nds, row_index) + total
+    directors_total << total
+    row_index += 1
+  end
+  result = {
+    "Stephen Spielberg" => directors_total[0],
+    "Russo Brothers" => directors_total[1],
+    "James Cameron" => directors_total[2],
+    "Spike Lee" => directors_total[3],
+    "Wachowski Siblings" => directors_total[4],
+    "Robert Zemeckis" => directors_total[5],
+    "Quentin Tarantino" => directors_total[6],
+    "Martin Scorsese" => directors_total[7],
+    "Francis Ford Coppola" => directors_total[8]
+  }
   nil
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(director_data)
+def gross_for_director(director_data, row)
+  column_index = 0
+  directors_total = []
+    while column_index < director_data[row][:movies].length do
+      directors_total = director_data[row][:movies][column_index][:worldwide_gross] + directors_total
+      column_index += 1
+    end
+ return director_total
 end
